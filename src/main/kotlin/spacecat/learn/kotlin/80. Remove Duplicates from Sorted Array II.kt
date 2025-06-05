cb007383158888e91aa9bd9418550a7a72d73cfe
SpaceCat <1983company@gmail.com>
2025-06-05 18:10:34 -0500

    finished more leetcode question

---
+++ src/main/kotlin/spacecat/learn/kotlin/80. Remove Duplicates from Sorted Array II.kt
@@ -0,0 +1,45 @@
+package spacecat.learn.kotlin
+
+fun main() {
+    val nums = intArrayOf(0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4)
+    println(nums.joinToString())
+    removeDuplicatesb(nums)
+}
+
+fun removeDuplicates(nums: IntArray): Int {
+    var nonDupArrayRunner = 0
+    val numsSize = nums.size - 1
+    var count = 1
+
+    for (i in 1..numsSize) {
+        if (nums[i] == nums[nonDupArrayRunner]) {
+            count++
+            if (count <= 2)
+                nonDupArrayRunner++
+            nums[nonDupArrayRunner] = nums[i]
+        }
+        if (nums[i] != nums[nonDupArrayRunner]) {
+            count = 1
+            nonDupArrayRunner++
+            nums[nonDupArrayRunner] = nums[i]
+        }
+
+        println("non: $nonDupArrayRunner i :$i count : $count")
+        println(nums.joinToString())
+    }
+    println(nums.joinToString())
+    println(nonDupArrayRunner)
+    return nonDupArrayRunner + 1
+}
+
+fun removeDuplicatesb(nums: IntArray): Int {
+    var k=1
+    for (i in 1 until nums.size) {
+        if (k == 1 || nums[i] != nums[k-2]) {
+            nums[k++] = nums[i]
+
+        }
+    }
+    println(nums.joinToString (  ))
+    return k
+}
