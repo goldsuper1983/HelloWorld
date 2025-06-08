9476165c541781e016558f1a1f74b589301862e7
SpaceCat <1983company@gmail.com>
2025-06-08 16:18:36 -0500

    solved more problems

--- src/main/kotlin/spacecat/learn/kotlin/189. Rotate Array.kt
+++ src/main/kotlin/spacecat/learn/kotlin/189. Rotate Array.kt
@@ -1,2 +1,36 @@
 package spacecat.learn.kotlin
 
+fun main() {
+    val nums = intArrayOf(1,2,3,4,5,6,7,8,9,10)
+    println(nums.joinToString ())
+    rotate(nums, 4)
+    println(nums.joinToString())
+}
+
+fun rotate(nums: IntArray, k: Int) {
+    val size = nums.size - 1
+    val insideK = if (k < nums.size) k else (k % nums.size)
+    val tempArray = IntArray(insideK)
+    if (size > 0) {
+        for (i in 0 until insideK) {
+            var j = size - i
+            val t = nums[j]
+            while (j - insideK >= 0) {
+                nums[j] = nums[j - insideK]
+                j -= insideK
+            }
+            tempArray[i] = t
+//            println(tempArray.joinToString ())
+//        println(nums.joinToString(":"))
+        }
+        for (i in 0 until insideK) {
+            nums[insideK - i - 1] = tempArray[i]
+        }
+//        println(nums.joinToString(":"))
+    }
+}
+
+
+
+
+
