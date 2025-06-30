c6755f1145be5d190431bc5e85debaba013e0851
SpaceCat <1983company@gmail.com>
2025-06-30 02:14:53 -0500

    solved more problems

--- src/main/kotlin/spacecat/learn/kotlin/55. Jump Game.kt
+++ src/main/kotlin/spacecat/learn/kotlin/55. Jump Game.kt
@@ -1,11 +1,17 @@
 package spacecat.learn.kotlin
 
 fun main(){
-    val nums = intArrayOf(2,3,1,1,4)
+    val nums = intArrayOf(2,5,0,1,4)
+    println(canJump(nums))
 }
 
 fun canJump(nums:IntArray): Boolean{
-
-
-    return true
+    var runner = nums.size-2
+    var jumpPoint = nums.size-1
+    while( runner >=0){
+        if(nums[runner]>=jumpPoint-runner) jumpPoint = runner
+        println("jump points =${nums[jumpPoint]} i = $jumpPoint")
+        runner--
+    }
+    return jumpPoint==0
 }
